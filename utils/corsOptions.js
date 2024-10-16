85058cf8c6d19dc2e07c2a3af93339294d622588
Kamit6337 <kamit6337@gmail.com>
2024-10-17 00:31:05 +0530

    update cors options

--- utils/corsOptions.js
+++ utils/corsOptions.js
@@ -1,5 +1,8 @@
 export const corsOptions = {
-  origin: ["https://voosh-project-client.vercel.app", "http://localhost:5173"],
+  origin: [
+    "https://voosh-project-client.onrender.com",
+    "http://localhost:5173",
+  ],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true,
 };
